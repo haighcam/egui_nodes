396fa420b3600fba9dca598ebc1d6542da685076
João Capucho <jcapucho7@gmail.com>
2021-08-08 22:30:58 +0100

    Update to egui 0.13

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,5 +1,5 @@
 use eframe::{egui, epi};
-use egui_nodes::{Context, NodeConstructor, LinkArgs, PinArgs, PinShape, NodeArgs};
+use egui_nodes::{Context, LinkArgs, NodeArgs, NodeConstructor, PinArgs, PinShape};
 
 struct MyApp {
     ctx: Context,
@@ -9,26 +9,49 @@ struct MyApp {
 pub fn example_graph(ctx: &mut Context, links: &mut Vec<(usize, usize)>, ui: &mut egui::Ui) {
     // add nodes with attributes
     let nodes = vec![
-        NodeConstructor::new(0, NodeArgs{outline: Some(egui::Color32::LIGHT_BLUE), ..Default::default()})
-            .with_origin([50.0, 150.0].into())
-            .with_title(|ui| ui.label("Example Node A"))
-            .with_input_attribute(0, PinArgs{shape: PinShape::Triangle, ..Default::default()}, |ui| ui.label("Input"))
-            .with_static_attribute(1, |ui| ui.label("Can't Connect to Me"))
-            .with_output_attribute(2, PinArgs{shape: PinShape::TriangleFilled, ..Default::default()}, |ui| ui.label("Output")),
+        NodeConstructor::new(
+            0,
+            NodeArgs {
+                outline: Some(egui::Color32::LIGHT_BLUE),
+                ..Default::default()
+            },
+        )
+        .with_origin([50.0, 150.0].into())
+        .with_title(|ui| ui.label("Example Node A"))
+        .with_input_attribute(
+            0,
+            PinArgs {
+                shape: PinShape::Triangle,
+                ..Default::default()
+            },
+            |ui| ui.label("Input"),
+        )
+        .with_static_attribute(1, |ui| ui.label("Can't Connect to Me"))
+        .with_output_attribute(
+            2,
+            PinArgs {
+                shape: PinShape::TriangleFilled,
+                ..Default::default()
+            },
+            |ui| ui.label("Output"),
+        ),
         NodeConstructor::new(1, Default::default())
             .with_origin([225.0, 150.0].into())
             .with_title(|ui| ui.label("Example Node B"))
             .with_static_attribute(3, |ui| ui.label("Can't Connect to Me"))
             .with_output_attribute(4, Default::default(), |ui| ui.label("Output"))
-            .with_input_attribute(5, Default::default(), |ui| ui.label("Input"))
+            .with_input_attribute(5, Default::default(), |ui| ui.label("Input")),
     ];
-    
+
     ctx.show(
         nodes,
-        links.iter().enumerate().map(|(i, (start, end))| (i, *start, *end, LinkArgs::default())),
-        ui
+        links
+            .iter()
+            .enumerate()
+            .map(|(i, (start, end))| (i, *start, *end, LinkArgs::default())),
+        ui,
     );
-    
+
     // remove destroyed links
     if let Some(idx) = ctx.link_destroyed() {
         links.remove(idx);
@@ -44,7 +67,7 @@ impl Default for MyApp {
     fn default() -> Self {
         Self {
             ctx: Context::default(),
-            links: Vec::new()
+            links: Vec::new(),
         }
     }
 }
@@ -66,5 +89,5 @@ impl epi::App for MyApp {
 }
 
 fn main() {
-    eframe::run_native(Box::new(MyApp::default()));
+    eframe::run_native(Box::new(MyApp::default()), eframe::NativeOptions::default());
 }
